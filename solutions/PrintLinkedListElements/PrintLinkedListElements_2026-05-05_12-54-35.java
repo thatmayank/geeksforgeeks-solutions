2caf6159f502979155a8df6c119d1fb0453e8523
Mayank <63830975+thatmayank@users.noreply.github.com>
2026-05-05 12:54:35 +0530

    add: PrintLinkedListElements_2026-05-05_12-54-35.java

---
+++ solutions/PrintLinkedListElements/PrintLinkedListElements_2026-05-05_12-54-35.java
@@ -0,0 +1,20 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public ArrayList<Integer> printList(Node head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        while(head != null) {
+            list.add(head.data);
+            head = head.next;
+        }
+        return list;
+    }
+}
